db4736d937e4da91fce9c012d7454b17ecc883d0
Mateusz Kowalik <msz.kowalik@gmail.com>
2020-12-06 17:59:04 +0100

    changed api url

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: "https://partkeeper-api.azurewebsites.net"
-  apiUrl: "http://locahost:7071/api"
+  apiUrl: "https://partkeeper-api.azurewebsites.net/api"
+  //apiUrl: "http://locahost:7071/api"
 };
